a59dc6de650e77ef09938aebf2927e431202dd51
cigui-PC\cigui <cgcg96@sjtu.edu.cn>
2018-06-05 18:37:28 +0800

    update sql

---
+++ sql/TraderTables.sql
@@ -0,0 +1,59 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2018/6/5 18:32:02                            */
+/*==============================================================*/
+
+
+drop table if exists "Order";
+
+drop table if exists Product;
+
+drop table if exists Trader;
+
+/*==============================================================*/
+/* Table: "Order"                                               */
+/*==============================================================*/
+create table "Order"
+(
+   id                   int not null,
+   broker               varchar(20) not null,
+   code                 varchar(8) not null,
+   product              varchar(16) not null,
+   period               varchar(16) not null,
+   price                int not null,
+   quantity             int not null,
+   initiatorName        varchar(20) not null,
+   initiatorComp        varchar(20) not null,
+   initiatorSide        bool not null,
+   completionName       varchar(20),
+   completionComp       varchar(20),
+   completionTime       timestamp,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: Product                                               */
+/*==============================================================*/
+create table Product
+(
+   id                   int not null,
+   broker               varchar(20) not null,
+   code                 varchar(8) not null,
+   name                 varchar(16) not null,
+   period               varchar(16) not null,
+   measurement          varchar(8) not null,
+   level                int not null,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: Trader                                                */
+/*==============================================================*/
+create table Trader
+(
+   id                   int not null,
+   name                 varchar(20) not null,
+   password             varchar(32) not null,
+   primary key (id)
+);
+
